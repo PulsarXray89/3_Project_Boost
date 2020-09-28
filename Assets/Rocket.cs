77cbbe070d3d5e31d17f924f4349829493a759df
PulsarXray89 <63971094+PulsarXray89@users.noreply.github.com>
2020-09-28 08:08:58 -0500

    Initial commit

---
+++ Assets/Rocket.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Rocket : MonoBehaviour
+{
+    [SerializeField] float mainThrust = 300f;
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float levelLoadDelay = 2f;
+
+    [SerializeField] AudioClip mainEngineAudio;
+    [SerializeField] AudioClip deathAudio;
+    [SerializeField] AudioClip successAudio;
+
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem deathParticles;
+    [SerializeField] ParticleSystem successParticles;
+
+    enum State { Alive, Dying, Transcending};
+    [SerializeField] State state = State.Alive;
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
+    bool collisionsDisabled = false;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+    void Update()
+    {
+        if(state == State.Alive)
+        {
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
+
+        if(state == State.Dying || state == State.Transcending)
+        {
+            mainEngineParticles.Stop();
+        }
+
+        if (Debug.isDebugBuild)
+        {
+            RespondToDebugKeys();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(state != State.Alive || collisionsDisabled) { return; }
+
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                // Do nothing;
+                print("OK");
+                break;
+            case "Finish":
+                StartFinishSequence();
+                break;
+            default:
+                StartDeathSequence();
+                break;
+        }
+    }
+
+    private void RespondToDebugKeys()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            LoadNextLevel();
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            collisionsDisabled = !collisionsDisabled;
+        }
+    }
+    private void StartFinishSequence()
+    {
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(successAudio);
+        successParticles.Play();
+        Invoke("LoadNextLevel", levelLoadDelay);
+    }
+    private void StartDeathSequence()
+    {
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(deathAudio);
+        deathParticles.Play();
+        Invoke("LoadFirstLevel", levelLoadDelay);
+    }
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+    private void LoadNextLevel()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        int nextScene = currentScene + 1;
+
+        if (nextScene == SceneManager.sceneCountInBuildSettings)
+        {
+            LoadFirstLevel();
+        }
+        else
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+    private void RespondToThrustInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            ApplyThrust();
+        }
+        else
+        {
+            audioSource.Stop();
+            mainEngineParticles.Stop();
+        }
+    }
+    private void ApplyThrust()
+    {
+        float thrustSpeed = mainThrust * Time.deltaTime;
+        rigidBody.AddRelativeForce(Vector3.up * thrustSpeed);
+        if (!mainEngineParticles.isPlaying) { mainEngineParticles.Play(); }
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngineAudio);
+        }
+    }
+    private void RespondToRotateInput()
+    {
+        float rotationSpeed = rcsThrust * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.back * rotationSpeed);
+        }
+    }
+}
